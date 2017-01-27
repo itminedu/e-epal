a8cfdd7cf7fc9bebf15a926f85ecb8ecc3774900
Νίκος Κατσαούνος <nkatsaounos@sch.gr>
2017-01-27 09:29:06 +0200

    Update app.settings.ts

--- source/app.settings.ts
+++ source/app.settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
    public static get API_ENDPOINT(): string {
-       return 'http://localhost/drupal-8.2.5';
+       return 'http://localhost/dist';
 //       return 'http://eepal.dev/drupal';
 //       return 'http://eduslim2.minedu.gov.gr/drupal';
    }

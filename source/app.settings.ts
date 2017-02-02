4308ec12a38f990b160594c0378e005d0ad14015
Achilles <achilleas01@yahoo.gr>
2017-02-02 13:16:22 +0200

    add 4 components to source folder - change webpack.config.js --change app.ts --change main.ts

--- source/app.settings.ts
+++ source/app.settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
    public static get API_ENDPOINT(): string {
-      return 'http://localhost/dist';
+      return 'http://localhost/angproject/eepal-front/drupal';
 //       return 'http://localhost/drupal-8.2.5';
 //       return 'http://eepal.dev/drupal';
 //       return 'http://eduslim2.minedu.gov.gr/drupal';
---
+++ source/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'reg-navbar',
+  templateUrl: 'navbar.component.html',
+})
+export default class NavbarComponent {
+
+}

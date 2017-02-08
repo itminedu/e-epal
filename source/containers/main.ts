ccf7659060a51c29ce804c38688fd3e4541f47c2
Open Source Developer <ostdeveloper@OSDeveloperWorkstation.minedu.gov.gr>
2017-02-08 14:05:26 +0200

    Navbarchanges

--- source/containers/main.ts
+++ source/containers/main.ts
@@ -19,49 +19,6 @@ import {
 @Component({
   selector: 'main',
   template: `
-  <div class="row">
-    <div class="col-md-3">
-      <ul class="nav nav-pills nav-stacked">
-      <li [ngClass]="{active: path=='application-preview'}">
-        <a [routerLink]="['/application-preview']">Προεπισκόπηση</a>
-      </li>
-      <li [ngClass]="{active: path=='epal-class-select'}">
-        <a [routerLink]="['/epal-class-select']">Επιλογή Τάξης</a>
-      </li>
-      <li [ngClass]="{active: path=='sector-fields-select'}">
-        <a [routerLink]="['/sector-fields-select']">Επιλογή Τομέα</a>
-      </li>
-      <li [ngClass]="{active: path=='course-fields-select'}">
-        <a [routerLink]="['/course-fields-select']">Επιλογή Ειδικότητας</a>
-      </li>
-      <li [ngClass]="{active: path=='sectorcourses-fields-select'}">
-        <a [routerLink]="['/sectorcourses-fields-select']">Επιλογή Ειδικότητας με βάση τον τομέα</a>
-      </li>
-      <li [ngClass]="{active: path=='region-schools-select'}">
-        <a [routerLink]="['/region-schools-select']">Επιλογή Σχολείου</a>
-      </li>
-      <li [ngClass]="{active: path=='student-application-form-main'}">
-        <a [routerLink]="['/student-application-form-main']">Αίτηση μαθητή / Καταχώρηση προσωπικών στοιχείων</a>
-      </li>
-      <li [ngClass]="{active: path=='students-list'}">
-        <a [routerLink]="['/students-list']">Μαθητές</a>
-      </li>
-      <li [ngClass]="{active: path=='form3'}">
-        <a [routerLink]="['/form3']">Αίτηση</a>
-      </li>
-      </ul>
-    </div>
-    <div class="col-md-9">
-      <div class="panel panel-primary">
-        <div class="panel-heading">
-          <h3 class="panel-title">{{path || 'home' | camelcase }}</h3>
-        </div>
-        <div class="panel-body">
-          <router-outlet></router-outlet>
-        </div>
-      </div>
-    </div>
-  </div>
   <reg-header></reg-header>
   <reg-navbar></reg-navbar>
   <reg-main></reg-main>

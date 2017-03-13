5de2acdb268547f4c42f132ee98a767671d7d815
Haris Papadopoulos <haris.rnd@gmail.com>
2017-03-13 11:19:59 +0200

    app.settings local setup

--- source/app.settings.ts
+++ source/app.settings.ts
@@ -1,14 +1,6 @@
-export const API_ENDPOINT = 'http://localhost/angular/eepal-front/drupal';
+export const API_ENDPOINT = 'http://eepal.dev/drupal';
 export class AppSettings {
-
    public static get API_ENDPOINT(): string {
-      return 'http://localhost/angular/eepal-front/drupal';
-   //	     return 'http://localhost/angular/eepal-front/drupal';
-  //     return 'http://localhost/drupal';
-//   	     return 'http://localhost/angular/eepal-front/drupal';
-//       return 'http://localhost/angular/eepal-front/drupal';
-//       return 'http://eepal.dev/drupal';
-//       return 'http://localhost/drupal';
-//       return 'http://eduslim2.minedu.gov.gr/drupal';
+      return 'http://eepal.dev/drupal';
    }
 }

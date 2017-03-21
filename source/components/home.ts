394f6e3486be641ebccf8585a66553e8d1839b28
Haris Papadopoulos <haris.rnd@gmail.com>
2017-03-21 13:48:07 +0200

    code review-fixes, WIP:parent form

--- source/components/home.ts
+++ source/components/home.ts
@@ -19,18 +19,7 @@ import { API_ENDPOINT } from '../app.settings';
     template: `
   <div>
        <form [formGroup]="formGroup" method = "POST" action="{{apiEndPoint}}/oauth/login" #form>
-<!--            <input type="hidden" name="X-oauth-enabled" value="true"> -->
-
             <div *ngFor="let loginInfoToken$ of loginInfo$ | async; let i=index"></div>
-<!--                <div class="row">
-                    <div class="col-md-6">
-                        {{loginInfoToken$.auth_token}}
-                    </div>
-                    <div class="col-md-6">
-                        {{loginInfoToken$.auth_role}}
-                    </div>
-                </div>
-            </div> -->
             <div class="row" style="min-height: 300px; margin-top: 100px;">
 
             <div *ngIf="!authToken" class="col-md-8 offset-md-4">
@@ -39,21 +28,6 @@ import { API_ENDPOINT } from '../app.settings';
                 </button>
             </div>
             </div>
-            <!--
-            <div class="row">
-            <div *ngIf="authToken" class="col-md-7 col-md-offset-5">
-                <h3>Καλώς ήρθατε</h3>
-            </div>
-            </div>
-            <div class="row">
-            <div *ngIf="authToken" class="col-md-7 col-md-offset-5">
-                <input type="hidden" name="dologout" value="true">
-                <button type="submit" class="btn-primary btn-lg pull-center" (click)="form.submit()">
-                Αποσύνδεση<span class="glyphicon glyphicon-menu-right"></span>
-                </button>
-            </div>
-            </div>
-            -->
      </form>
   </div>
   `
@@ -63,8 +37,8 @@ export default class Home implements OnInit {
     public formGroup: FormGroup;
     private authToken: string;
     private authRole: string;
-    private name :any;
-    private xcsrftoken :any;
+    private name: any;
+    private xcsrftoken: any;
     private loginInfo$: Observable<ILoginInfo>;
     private apiEndPoint = API_ENDPOINT;
 
@@ -72,48 +46,43 @@ export default class Home implements OnInit {
         private _ata: LoginInfoActions,
         private _ngRedux: NgRedux<IAppState>,
         private activatedRoute: ActivatedRoute,
-        private _hds: HelperDataService, 
+        private _hds: HelperDataService,
         private router: Router
-        ) {
-            this.authToken = '';
-            this.authRole = '';
-            this.name ='';
+    ) {
+        this.authToken = '';
+        this.authRole = '';
+        this.name = '';
         this.formGroup = this.fb.group({
-//            Username: [],
-//            Paswd: []
         });
     };
 
     ngOnInit() {
-               this.loginInfo$ = this._ngRedux.select(state => {
+        this.loginInfo$ = this._ngRedux.select(state => {
             if (state.loginInfo.size > 0) {
                 state.loginInfo.reduce(({}, loginInfoToken) => {
                     this.authToken = loginInfoToken.auth_token;
                     this.authRole = loginInfoToken.auth_role;
-                     if (this.authToken && this.authToken.length > 0)
-                        this.router.navigate(['/epal-class-select']);
+                    if (this.authToken && this.authToken.length > 0)
+                        this.router.navigate(['/parent-form']);
                     return loginInfoToken;
                 }, {});
             }
 
             return state.loginInfo;
         });
-       
+
         // subscribe to router event
         this.activatedRoute.queryParams.subscribe((params: Params) => {
             if (params) {
                 this.authToken = params['auth_token'];
                 this.authRole = params['auth_role'];
             }
-         
+
             if (this.authToken && this.authRole)
-                this._ata.getloginInfo({ auth_token: this.authToken, auth_role: this.authRole});
-     
+                this._ata.getloginInfo({ auth_token: this.authToken, auth_role: this.authRole });
+
         });
     }
-
-
-
     checkvalidation() {
 
     }

5c33da95de17fd55c9f7f5429ff1f1a5bf3f8183
Open Source Developer <ostdeveloper@OSDeveloperWorkstation.minedu.gov.gr>
2017-03-22 16:13:17 +0200

    trans

---
+++ drupal/modules/epal/src/Controller/SubmitedApplications.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\epal\Controller;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SubmittedApplication extends ControllerBase
+{
+    protected $entityTypeManager;
+
+    public function __construct(EntityTypeManagerInterface $entityTypeManager)
+    {
+        $this->entityTypeManager = $entityTypeManager;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('entity_type.manager')
+        );
+    }
+
+
+    public function getSubmittedApplications(Request $request)
+    {
+  
+        return "aaaaaaaaaaaaaaaaa" ;
+    }
+
+
+  
+}

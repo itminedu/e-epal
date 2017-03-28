8066494057f63b8ce1d654631c8748348c50d7f6
Nikos <nkatsaounos@sch.gr>
2017-03-28 11:13:34 +0300

    added Distribution Controller, added createDemoData Controller, updated EpalStudentClass Entity

--- source/app.settings.ts
+++ source/app.settings.ts
@@ -1,6 +1,6 @@
-export const API_ENDPOINT = 'http://eepal.dev/drupal';
+export const API_ENDPOINT = 'http://localhost/drupal-8.2.6';
 export class AppSettings {
    public static get API_ENDPOINT(): string {
-      return 'http://eepal.dev/drupal';
+      return 'http://localhost/drupal-8.2.6';
    }
 }

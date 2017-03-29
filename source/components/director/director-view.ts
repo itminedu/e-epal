9744fe1671d15e97de2133e7022521064876d9ac
Open Source Developer <ostdeveloper@OSDeveloperWorkstation.minedu.gov.gr>
2017-03-29 23:18:00 +0300

    trans

--- source/components/director/director-view.ts
+++ source/components/director/director-view.ts
@@ -8,35 +8,60 @@ import { NgRedux, select } from 'ng2-redux';
 import { IAppState } from '../../store/store';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs/Rx';
-
+import {
+    FormBuilder,
+    FormGroup,
+    FormControl,
+    FormArray,
+    Validators,
+} from '@angular/forms';
 @Component({
     selector: 'director-view',
     template: `
           
-    
-    
-              <label for="name">Παρακαλώ επιλέξτε τάξη </label><br/>
-                    <select>
-                        <option value=1>Α' Λυκείου</option>
-                        <option value=2>Β' Λυκείου</option>
-                        <option value=3>Γ' Λυκείου</option>
+    <form [formGroup]="formGroup">
+         
+            <div class="form-group" style= "margin-top: 50px; margin-bottom: 100px;">
+              <label for="name">Τάξη</label><br/>
+                    <select class="form-control" formControlName="name" (change)="verifyclass()">
+                        <option value="Α' Λυκείου">Α' Λυκείου</option>
+                        <option value="Β' Λυκείου">Β' Λυκείου</option>
+                        <option value="Γ' Λυκείου">Γ' Λυκείου</option>
                     </select>
 
-           
+           <div>
+    
+        <div class="form-group" style= "margin-top: 50px; margin-bottom: 100px;">
+              <label for="name1">Τάξη1</label><br/>
+                    <select class="form-control" formControlName="name1" (change)="verifyclass()">
+                        <option value="Α' Λυκείου">Α' Λυκείου</option>
+                        <option value="Β' Λυκείου">Β' Λυκείου</option>
+                        <option value="Γ' Λυκείου">Γ' Λυκείου</option>
+                    </select>
+
+         <div>
+    
+               
    `
 })
 
 @Injectable() export default class DirectorView implements OnInit , OnDestroy{
 
-   
+    public formGroup: FormGroup;
     private StudentSelected$: BehaviorSubject<any>;
     private StudentSelectedSub: Subscription;
-       
-    constructor(private _hds: HelperDataService, 
+    public bClassEnabled: boolean;
+
+    constructor(private fb: FormBuilder,
+                private _hds: HelperDataService, 
                 private activatedRoute: ActivatedRoute,
                 private router: Router )
     {
        this.StudentSelected$ = new BehaviorSubject([{}]);
+       this.formGroup = this.fb.group({
+                name:[],
+                name1 :[]
+                 });
     }
 
     ngOnDestroy()
@@ -48,11 +73,16 @@ import { BehaviorSubject, Subscription } from 'rxjs/Rx';
  
     ngOnInit() {
      
-   
-           
+       this.bClassEnabled = false;
+               
 
     }
 
+
+    verifyclass()
+    {
+       this.bClassEnabled = true;
+    }
 
 
 }

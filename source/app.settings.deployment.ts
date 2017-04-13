cdd54b2f99c25dd5c1b337f07806c01008f06c58
Nikos <nkatsaounos@sch.gr>
2017-04-14 01:14:19 +0300

    updated settings file

--- source/app.settings.deployment.ts
+++ source/app.settings.deployment.ts
@@ -2,6 +2,6 @@ export const API_ENDPOINT = 'http://localhost/drupal-8.2.6';
 export class AppSettings {
 
    public static get API_ENDPOINT(): string {
-       return 'http://localhost/drupal-8.2.5';
+       return 'http://localhost/drupal-8.2.6';
    }
 }

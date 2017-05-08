3fa4fc2f8b5ae2d0ced1016b644a45f816ad0e57
Open Source Developer <ostdeveloper@OSDeveloperWorkstation.minedu.gov.gr>
2017-05-08 16:10:34 +0300

    trans

--- source/components/student-application-form/submited.aplication.preview.ts
+++ source/components/student-application-form/submited.aplication.preview.ts
@@ -12,6 +12,14 @@ import { IAppState } from '../../store/store';
 import { ILoginInfo } from '../../store/logininfo/logininfo.types';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import { BehaviorSubject, Subscription } from 'rxjs/Rx';
+import {
+    FormBuilder,
+    FormGroup,
+    FormControl,
+    FormArray,
+    Validators,
+} from '@angular/forms';
+
 
 
 @Component({ 
@@ -32,7 +40,89 @@ import { BehaviorSubject, Subscription } from 'rxjs/Rx';
                
 
 
-            <div *ngFor="let StudentDetails$  of SubmitedDetails$ | async" [hidden]="UserData$.id !== userActive" >
+                  <div *ngFor="let StudentDetails$  of SubmitedDetails$ | async" [hidden]="UserData$.id !== userActive" >
+                       <form [formGroup]="studentDataGroup" >
+                                    <form [formGroup]="applicantDataGroup">
+                                      <table>
+                                        <tr><td>
+                                          <div class="form-group">
+                                            <label for="guardianfirstname">Όνομα κηδεμόνα</label><input class="form-control" type="text" formControlName="guardianfirstname" disabled = "true">
+                                          </div>
+                                        </td>
+                                        <td>
+                                          <div class="form-group">
+                                            <label for="guardiansurname">Επώνυμο κηδεμόνα</label><input class="form-control" type="text" formControlName="guardiansurname" disabled = "true">
+                                          </div>
+                                        </td></tr>
+                                      </table>
+                                    </form>
+
+                                    <div class="form-group">
+                                        <label for="name">Όνομα μαθητή</label><input class="form-control" type="text" formControlName="name">
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <label for="studentsurname">Επώνυμο μαθητή</label><input class="form-control" type="text" formControlName="studentsurname">
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <label for="fatherfirstname">Όνομα πατέρα</label><input class="form-control" type="text" formControlName="fatherfirstname">
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <label for="fathersurname">Επώνυμο πατέρα</label><input class="form-control" type="text" formControlName="fathersurname">
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <label for="motherfirstname">Όνομα μητέρας</label><input class="form-control" type="text" formControlName="motherfirstname">
+                                    </div>
+                                   
+                                    <div class="form-group">
+                                        <label for="mothersurname">Γένος μητέρας</label><input class="form-control" type="text" formControlName="mothersurname">
+                                    </div>
+                                   
+                                   
+                                  <div class="form-group">
+                                      <label for="studentbirthdate">Ημερομηνία γέννησης</label>
+                                      <input class="form-control" type="date" formControlName="studentbirthdate">
+                                  </div>
+
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                <div class="form-group">
+                                                    <label for="regionaddress">Διεύθυνση κατοικίας</label><input class="form-control" type="text" formControlName="regionaddress">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="form-group">
+                                                    <label for="regiontk">TK </label><input class="form-control" type="text" formControlName="regiontk">
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="form-group">
+                                                    <label for="regionarea">Πόλη/Περιοχή</label><input class="form-control" type="text" formControlName="regionarea">
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+
+
+                                    <div class="form-group">
+                                        <label for="telnum">Τηλέφωνο επικοινωνίας</label><input class="form-control" type="text" formControlName="telnum">
+                                    </div>
+
+                                 
+                                  </form>
+
+
+
+                                 
+
+                          
+
+
+
                  <strong> Όνομα:</strong>    <p style="border:1px solid #eceeef;">    {{StudentDetails$.name}} </p>
                  <strong> Επώνυμο:</strong>    <p style="border:1px solid #eceeef;">    {{StudentDetails$.studentsurname}} </p>
                  <strong> Όνομα Πατέρα:</strong>  <p style="border:1px solid #eceeef;"> {{StudentDetails$.fatherfirstname}}</p>
@@ -42,7 +132,7 @@ import { BehaviorSubject, Subscription } from 'rxjs/Rx';
                  <strong> Ημερομηνία Γέννησης:</strong> <p style="border:1px solid #eceeef;">{{StudentDetails$.birthdate}}</p>
                  <p><b>Επιλογές ΕΠΑΛ</b> </p>
                  <br>
-             </div>
+               </div>
                 
                 <div *ngFor="let epalChoices$  of EpalChosen$ | async" [hidden]="UserData$.id !== userActive">
                      Σχολείο: {{epalChoices$.epal_id}}
@@ -72,17 +162,36 @@ import { BehaviorSubject, Subscription } from 'rxjs/Rx';
 
     private EpalChosen$: BehaviorSubject<any>;
     private EpalChosenSub: Subscription;
+    public studentDataGroup: FormGroup;
+    public applicantDataGroup: FormGroup;
+
 
     public StudentId;
     private userActive = <number>-1;
 
     constructor(private _hds: HelperDataService,
                 private activatedRoute: ActivatedRoute,
-                private router: Router )
+                private router: Router ,
+                private fb: FormBuilder)
     {
        this.SubmitedApplic$ = new BehaviorSubject([{}]);
        this.SubmitedDetails$ = new BehaviorSubject([{}]);
        this.EpalChosen$ = new BehaviorSubject([{}]);
+
+      this.studentDataGroup = this.fb.group({
+            epaluser_id: [,[]],
+            name: ['ΝΙΚΟΣ'],
+            studentsurname: ['ΚΑΤΣΑΟΥΝΟΣ'],
+            studentbirthdate: [''],
+        });
+
+        this.applicantDataGroup = this.fb.group({
+          guardianfirstname: ['ΑΝΑΣΤΑΣΙΟΣ'],
+          guardiansurname: ['ΚΑΤΣΑΟΥΝΟΣ'],
+        });
+
+
+
 
     }
 
